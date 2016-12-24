81f3dc1e496e80f65297c07fcd4cdfad19923183
Jowee Caquicla <iamelixirdayo@gmail.com>
2016-12-25 03:19:20 +0800

    login and signing in

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,31 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+       Route::post('/signup', [
+        'uses' => 'UserController@postSignUp',
+        'as' => 'signup'
+       ]);
+
+       Route::post('/signin', [
+               'uses' => 'UserController@postSignIn',
+               'as' => 'signin'
+              ]);
+
+       Route::get('/dashboard', [
+       'uses' => 'UserController@getDashboard',
+       'as' => 'dashboard'
+       ]);
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,44 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+@extends('layouts.master')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+@section('title')
+    Welcome!
+@endsection
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
+@section('content')
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Sign Up</h3>
+            <form action="{{ route('signup') }}" method="post">
+                <div class="form-group">
+                    <label for="email">Your E-Mail</label>
+                    <input class="form-control" type="text" name="email" id="email"></input>
+                </div>
+                <div class="form-group">
+                    <label for="first_name">Your First Name</label>
+                    <input class="form-control" type="text" name="first_name" id="first_name"></input>
+                </div>
+                <div class="form-group">
+                    <label for="password">Your Password</label>
+                    <input class="form-control" type="password" name="password" id="password"></input>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                <input type="hidden" name="_token" value="{{ Session::token() }}">
+            </form>
         </div>
-    </body>
-</html>
+        <div class="col-md-6">
+            <h3>Sign In</h3>
+            <form action="{{ route('signin') }}" method="post">
+                <div class="form-group">
+                    <label for="email">Your E-Mail</label>
+                    <input class="form-control" type="text" name="email" id="email"></input>
+                </div>
+                <div class="form-group">
+                    <label for="password">Your Password</label>
+                    <input class="form-control" type="password" name="password" id="password"></input>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                <input type="hidden" name="_token" value="{{ Session::token() }}">
+            </form>
+        </div>
+    </div>
+@endsection
