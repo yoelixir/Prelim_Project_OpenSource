2df913d2faacc339b2d3eb6252e0b023603b0bc5
Jowee Caquicla <iamelixirdayo@gmail.com>
2017-01-13 18:46:45 +0800

    another fix on image upload

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -76,16 +76,28 @@ class UserController extends Controller
         ]);
 
         $user = Auth::user();
+        $old_name = $user->first_name;
         $user->first_name = $request['first_name'];
         $user->last_name = $request['last_name'];
         $user->email = $request['email'];
         $user->phone = $request['phone'];
-        $password = bcrypt($request['password']);
+        $user->password = bcrypt($request['password']);
         $user->update();
         $file = $request->file('image');
         $filename = $request['first_name'] . '-' . $user->id . '.jpg';
+        $old_filename = $old_name . '-' . $user->id . '.jpg';
+        $update = false;
+
+        if (Storage::disk('local')->has($old_filename)) {
+            $old_file = Storage::disk('local')->get($old_filename);
+            Storage::disk('local')->put($filename, $old_file);
+            $update = true;
+        }
         if ($file) {
             Storage::disk('local')->put($filename, File::get($file));
+        }
+        if ($update && $old_filename !== $filename) {
+            Storage::delete($old_filename);
         }
         return redirect()->route('account');
     }
